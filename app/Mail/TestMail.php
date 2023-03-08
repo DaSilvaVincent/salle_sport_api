1a6d62fc3c1f32d2f0ee2180e03bf28eb1122935
Vincent DaSilva <vincent.dasilva@ens-univ.artois.fr>
2023-03-08 14:44:48 +0100

    Modif

---
+++ app/Mail/TestMail.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Mail;
+
+// use ...
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Address;
+use Illuminate\Mail\Mailables\Attachment;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+
+class TestMail extends Mailable {
+    use Queueable, SerializesModels;
+
+    public $details;
+    public $filename;
+
+    public function __construct($details, $filename) {
+        Log::info("Prepare message");
+        $this->details = $details;
+        $this->filename = $filename;
+    }
+
+    public function envelope() {
+        return new Envelope(
+            from: new Address('Admin@appli.com', 'Gerard Martin'),
+            subject: 'Liste des personnes', );
+    }
+
+    public function content() {
+        return new Content(
+            view: 'emails.test',
+        );
+    }
+
+    public function attachments() {
+        return [
+            Attachment::fromStorageDisk('public', $this->filename),
+        ];
+    }
+}
