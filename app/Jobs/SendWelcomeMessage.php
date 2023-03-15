51c544550e21c77c056792f6166bc862515fb7a2
Vincent DaSilva <vincent.dasilva@ens-univ.artois.fr>
2023-03-15 15:11:33 +0100

    Début envoie de mail

--- app/Jobs/SendWelcomeMessage.php
+++ app/Jobs/SendWelcomeMessage.php
@@ -2,39 +2,38 @@
 
 namespace App\Jobs;
 
+use App\Mail\TestMail;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
 
 class SendWelcomeMessage implements ShouldQueue {
 
-//    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
-//
-//    public $users;
-//
-//    /**
-//     * Create a new job instance.
-//     *
-//     * @return void
-//     */
-//    public function __construct($users){
-//        $this->users = $users;
-//    }
-//
-//    /**
-//     * Execute the job.
-//     *
-//     * @return void
-//     */
-//    public function handle()
-//    {   $this->users => mail
-//        return $this->from("bonjour@gmail.com") // L'expéditeur
-//        ->subject("Message welcome") // Le sujet
-//        ->view('clients.bienvenue'); // La vue
-//    }
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(){
+        Log::info("Send welcome");
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle() {
+        // Transmission d'un mail contenant le fichier calculé
+        Mail::to("robert.duchmol@domain.fr")
+        ->send(new TestMail());
+    }
 
 }
 #mhsendmail
