4a2489a89a4a71f845ac102368716523f98c4269
thimothee.lepetz <thimothee.lepetz@info0702.ipa.iutlens.local>
2023-03-22 09:20:59 +0100

    ajout provider update-client

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+use App\Models\Client;
+use App\Models\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
